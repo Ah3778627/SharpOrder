1b14e67f073678ba772a7b78da38af001dfcb55a
Abdul Hafiz <abdul-hafiz@example.com>
2025-12-02 10:43:54 +0000

    Enhance storefront: wishlist, toast notifications, coupon support, layout updates

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
 #write
 #  a function that prints hello whenever it's called
 tuples = ()
+purchase = 6.7
 name = 'John'
 #args is short for arguments
 #kwargs is short for key word arguments
@@ -8,7 +9,19 @@ def at(*args):
     
     print(f'hello {args}')
 
-at(3)
+
 def art():
     print('good morning')
-art()
+
+
+purchase_amout = 75.6
+if purchase_amout >= 100:
+    discount = 0.15
+    payment_amount = purchase_amout - (purchase_amout * discount)
+    print(f'you have a discount of {discount*100}%. your total payment amount is {payment_amount}')
+elif purchase_amout >= 50 and purchase_amout < 100:
+    discount = 0.05
+    payment_amount = purchase_amout - (purchase_amout * discount)
+    print(f'you have a discount of {discount*100}%. your total payment amount is {payment_amount}')
+else:
+    print(f'your total payment amount is {purchase_amout}')
